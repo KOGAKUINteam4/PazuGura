c26922e3278864d71f8e4d6983f2afc10c54787a
nishi <icemaunntenn@gmail.com>
2015-09-25 16:32:58 +0900

    add nishi file

---
+++ PazuGra/Assets/Nishi/nishitest.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class nishitest : MonoBehaviour {
+
+    private PolygonCollider2D poly;
+
+    // Use this for initialization
+    void Start () {
+        poly = gameObject.GetComponent<PolygonCollider2D>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //ポリゴンコリジョン２Dを変更する
+        poly.points = new Vector2[]
+        {
+            new Vector2(1,1),
+            new Vector2(2,1),
+            new Vector2(1,2),
+            new Vector2(2,2),
+        };
+    }
+}
