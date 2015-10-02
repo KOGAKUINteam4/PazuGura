026701cdae19fe297e1e351d29b3834e3b365303
nishi <icemaunntenn@gmail.com>
2015-10-02 09:47:06 +0900

    add testTitle

---
+++ PazuGra/Assets/Nishi/Script/TouchPoint.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchPoint : MonoBehaviour {
+
+    public GameObject obj;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 screenPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            screenPos.z = 0;
+            Instantiate(obj, screenPos,Quaternion.Euler(-90,0,0));
+        }
+	}
+}
