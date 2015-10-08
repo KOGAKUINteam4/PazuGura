de10bf9e1c6fb9e8f7804b05e90e54bd7750ef98
nishi <icemaunntenn@gmail.com>
2015-10-08 19:52:22 +0900

    Add Gyro

---
+++ PazuGra/Assets/Nishi/Script/GyroGravity.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GyroGravity : MonoBehaviour {
+
+    public float acceleration = 1;
+
+	// Use this for initialization
+	void Start () {
+        Input.gyro.enabled = true;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Physics2D.gravity = Input.gyro.gravity * acceleration;
+	}
+}
