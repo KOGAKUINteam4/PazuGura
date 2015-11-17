3f289a3dd297b95cc1541b2499edaea777942489
nishi <icemaunntenn@gmail.com>
2015-11-17 17:43:29 +0900

    修正

---
+++ PazuGra/Assets/Nishi/Script/Effects/EffectManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EffectManager : MonoBehaviour {
+    private static EffectManager sInstance;
+
+    public static EffectManager Instance
+    {
+        get
+        {
+            if (sInstance == null)
+                sInstance = GameObject.FindObjectOfType<EffectManager>();
+            return sInstance;
+        }
+    }
+
+    public void Create(GameObject prefab)
+    {
+        Instantiate(prefab);
+    }
+
+    public void Create(Vector3 position, GameObject prefab)
+    {
+        Instantiate(prefab, position, Quaternion.identity);
+    }
+}
---
+++ PazuGra/Assets/Nishi/Script/Effects/MainCallEffect.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MainCallEffect : MonoBehaviour {
+
+    [SerializeField]
+    Image m_Hole;
+    [SerializeField]
+    Image m_Shadow;
+
+    RectTransform m_Rectpos;
+
+    // Use this for initialization
+    void Start () {
+        m_Rectpos = gameObject.GetComponent<RectTransform>();
+
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Color color = new Color(0.0f, 0.0f,0.0f,0.01f);
+
+        m_Hole.color += color;
+        m_Shadow.color += color;
+
+        if (m_Hole.color.a >= 1)
+        {
+            if (gameObject.transform.position.y <= -100)
+            {
+                m_Hole.rectTransform.localScale += new Vector3(0.1f, 0.1f, 0.0f);
+            }
+            else
+            {
+                Vector3 pos = new Vector3(0.0f, -1.0f, 0.0f);
+                gameObject.transform.position += pos;
+            }
+        }
+	
+	}
+}
