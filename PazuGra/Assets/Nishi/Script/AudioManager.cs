cbe521d612814891c5455494429be798bb329978
nishi <icemaunntenn@gmail.com>
2015-12-02 20:11:42 +0900

    サウンドマネージャー追加

---
+++ PazuGra/Assets/Nishi/Script/AudioManager.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AudioManager : MonoBehaviour
+{
+
+    private static AudioManager sInstance;
+
+    public static AudioManager Instance
+    {
+        get
+        {
+            if (sInstance == null)
+                sInstance = GameObject.FindObjectOfType<AudioManager>();
+            return sInstance;
+        }
+    }
+
+    [SerializeField]
+    private AudioSource m_AudioSE;
+    [SerializeField]
+    private AudioSource m_AudioBGM;
+
+    public Dictionary<string, AudioClip> m_AudioSEClips = new Dictionary<string, AudioClip>();
+    public Dictionary<string, AudioClip> m_AudioBGMClips = new Dictionary<string, AudioClip>();
+
+    public void Awake()
+    {
+    }
+
+    public void BGMPlay(string name)
+    {
+        m_AudioBGM.clip = m_AudioBGMClips[name];
+        m_AudioBGM.Play();
+    }
+
+    public void BGMStop()
+    {
+        m_AudioBGM.Stop();
+    }
+
+    public void SEPlay(string name)
+    {
+        m_AudioSE.PlayOneShot(m_AudioSEClips[name]);
+    }
+
+}
