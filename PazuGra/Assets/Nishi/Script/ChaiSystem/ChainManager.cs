c516ca3cebf2fa4c2d6caf2548eab565f056f8b5
nishi <icemaunntenn@gmail.com>
2015-12-06 10:27:44 +0900

    作業途中

--- PazuGra/Assets/Nishi/Script/ChaiSystem/ChainManager.cs
+++ PazuGra/Assets/Nishi/Script/ChaiSystem/ChainManager.cs
@@ -16,6 +16,7 @@ public class ChainManager : MonoBehaviour
     private int m_Maxchain = 0;
     private bool m_isClick = false;  //クリックが完了したか
 
+    private bool m_isListUp = false;  //削除リストが完了したか
     private GameObject m_ClickObj;
 
     // Update is called once per frame
@@ -24,24 +25,28 @@ public class ChainManager : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             CrickStart();
-            //PushList();
+            m_isListUp = true;
 
         }
-        if(Input.GetMouseButton(0))
+        if (Input.GetMouseButton(0))
         {
-            
+            if (m_isListUp)
+            {
+                PushList();
+                m_isListUp = false;
+            }
         }
         if (Input.GetMouseButtonUp(0) && m_isClick)
         {
-            PushList();
+
             //ColliderSwitch(true);
             //PushList();
             var layerMask = 1 << LayerMask.NameToLayer("Block");
             RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero, 1.0f, layerMask);
             //if(hit.collider.gameObject == m_ClickObj)
             //{
-                Remove();
-                m_isClick = false;
+            Remove();
+            m_isClick = false;
             //}  
         }
 
@@ -68,14 +73,14 @@ public class ChainManager : MonoBehaviour
                     hitObj.GetComponent<Chain>().SetCheck(true);
                     m_isClick = true;
                 }
-                
+
             }
         }
     }
 
     void PushList()
     {
-        
+
         var objs = GameObject.FindGameObjectsWithTag("Collider");
         foreach (GameObject obj in objs)
         {
@@ -106,7 +111,7 @@ public class ChainManager : MonoBehaviour
             gameManager.GetScoreUI().UpdateScore(score);
             gameManager.GetScoreUI().CreateEffect();
             ComboGaugeStart();
-            if(m_removeList.Count >= 5)
+            if (m_removeList.Count >= 5)
             {
                 ExecuteEvents.Execute<IRecieveMessage>(
                     m_BlockFactory, // 呼び出す対象のオブジェクト
@@ -175,7 +180,7 @@ public class ChainManager : MonoBehaviour
             }
         }
 
-        foreach(GameObject obj in m_FlashList)
+        foreach (GameObject obj in m_FlashList)
         {
             obj.transform.parent.GetChild(0).GetComponent<Image>().color = new Color(0, 0, 0, 0);
         }
