551f34fefeea983e063ad4933c64a424b2eaccab
nishi <icemaunntenn@gmail.com>
2016-01-22 15:56:37 +0900

    レイアウトの差し替え

--- PazuGra/Assets/Nishi/Script/Effects/TitleLogo.cs
+++ PazuGra/Assets/Nishi/Script/Effects/TitleLogo.cs
@@ -8,26 +8,28 @@ public class TitleLogo : MonoBehaviour {
 	void Start ()
     {
         Image image = transform.GetChild(0).gameObject.GetComponent<Image>();
-        LeanTween.alpha(image.rectTransform, 1, 1).setOnComplete(() => { MenuMove(); });
+        Image image1 = transform.GetChild(1).gameObject.GetComponent<Image>();
+        LeanTween.alpha(image.rectTransform, 1, 1);
+        LeanTween.alpha(image1.rectTransform, 1, 1).setOnComplete(() => { MenuMove(); });
         //LeanTween.moveLocalY(transform.GetChild(2).gameObject,-100,1.0f).setOnComplete(() => { Rainbow(); });
-	}
+    }
 
     void Rainbow()
     {
-        Image image = transform.GetChild(0).gameObject.GetComponent<Image>();
+        Image image = transform.GetChild(1).gameObject.GetComponent<Image>();
         LeanTween.value(gameObject, 0, 1, 1).setOnUpdate((float val) => { image.fillAmount = val; }).setOnComplete(() => { PenMove(); });
 
     }
 
     void PenMove()
     {
-        LeanTween.moveLocal(transform.GetChild(1).gameObject,new Vector3(350,-70,0), 1.0f).setOnComplete(() => { MenuMove();});
+        LeanTween.moveLocal(transform.GetChild(2).gameObject,new Vector3(350,-70,0), 1.0f).setOnComplete(() => { MenuMove();});
     }
 
     void MenuMove()
     {
-        LeanTween.moveLocalX(transform.GetChild(1).gameObject, 0, 1.0f);
         LeanTween.moveLocalX(transform.GetChild(2).gameObject, 0, 1.0f);
+        LeanTween.moveLocalX(transform.GetChild(3).gameObject, 0, 1.0f);
     }
 
     void Update()
@@ -35,10 +37,12 @@ public class TitleLogo : MonoBehaviour {
         if (Input.GetMouseButtonDown(0))
         {
             LeanTween.cancelAll();
-            Image image = transform.GetChild(0).gameObject.GetComponent<Image>();
+            Image image = transform.GetChild(1).gameObject.GetComponent<Image>();
             image.color = new Color(1, 1, 1, 1);
-            transform.GetChild(1).localPosition = new Vector3(0, -627.653f, 0);
-            transform.GetChild(2).localPosition = new Vector3(0, -1048, 0);
+            image = transform.GetChild(0).gameObject.GetComponent<Image>();
+            image.color = new Color(image.color.r, image.color.g, image.color.b, 1);
+            transform.GetChild(2).localPosition = new Vector3(0, -627.653f, 0);
+            transform.GetChild(3).localPosition = new Vector3(0, -1048, 0);
             //transform.GetChild(1).GetComponent<RectTransform>().position = new Vector3(350, -70, 0);
             //transform.GetChild(2).GetComponent<RectTransform>().position = new Vector3(0, -100, 0);
             //transform.GetChild(3).GetComponent<RectTransform>().position = new Vector3(0, -627.653f, 0);
---
+++ PazuGra/Assets/Nishi/Script/Raibow.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Raibow : MonoBehaviour
+{
+
+    public Color[] m_Colors;
+    private Color m_NextColor;
+    private float t;
+    private int i = 0;
+
+    public void Start()
+    {
+        m_NextColor = m_Colors[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        t += Time.deltaTime / 2;
+        if (t > 0.7)
+        {
+            t = 0;
+            i++;
+            if (i >= m_Colors.Length) i = 0;
+            m_NextColor = m_Colors[i];
+        }
+
+        GetComponent<Image>().color = Color.Lerp(GetComponent<Image>().color, m_NextColor,t);
+
+    }
+}
