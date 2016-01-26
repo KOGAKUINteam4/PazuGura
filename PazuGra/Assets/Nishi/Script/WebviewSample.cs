77ce232c21cae0ec478e9b733379ca9b6b1f2cd6
nishi <icemaunntenn@gmail.com>
2016-01-26 15:14:59 +0900

    画像の追加

---
+++ PazuGra/Assets/Nishi/Script/WebviewSample.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WebviewSample : MonoBehaviour
+{
+
+    Vector3 defaultScale = Vector3.zero;
+
+    public void Start()
+    {
+        defaultScale = transform.lossyScale;
+    }
+
+    public void Update()
+    {
+        Vector3 lossScale = transform.lossyScale;
+        Vector3 localScale = transform.localScale;
+        transform.localScale = new Vector3(
+                localScale.x / lossScale.x * defaultScale.x,
+                localScale.y / lossScale.y * defaultScale.y,
+                localScale.z / lossScale.z * defaultScale.z);
+    }
+
+
+
+    // Use this for initialization
+    public void GoURL()
+    {
+        Application.OpenURL("https://www.youtube.com/watch?v=7cwvUiGBMm4");
+    }
+
+}
